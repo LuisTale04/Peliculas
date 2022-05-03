3dc3644ef027ab6f1996d1fc50395aedff17d115
Luis Alexander Talé de León <100502736+LuisTale04@users.noreply.github.com>
2022-05-03 09:48:31 -0600

    animal fav

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print('Nombre: Luis Alexander Talé de León \nCarné: 1507722\nCurso: Introducción a la ingeniería en informatica y sistemas')
+
+animal = input('Ingrese un animal: ')
+ 
+if animal == 'Perro':
+    print('Es mi animal favorito támbien, juega y corre, además de ser un compañero fiel')
+
+else: 
+    print('Me parece un animal bueno e interesante, pero realmente me gustan mas los perros') 
